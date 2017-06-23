6e863a81f5b9dc71043d644665e44142e7a5d80b
Kedan Li <likedan5@icloud.com>
2017-06-23 08:43:42 -0700

    Multiaccount oauth2 backgroundSigninUserSilently

--- MultiAccountOauth/GoogleUserInstance.swift
+++ MultiAccountOauth/GoogleUserInstance.swift
@@ -13,7 +13,7 @@ public struct GoogleUserInstance {
     public var id: String
     public var profile: String
     public var refresh_token: String
-    public var id_token: String?
-    public var access_token: String?
+    public var id_token: String
+    public var access_token: String
     public var server_token: String?
 }
--- MultiAccountOauth/OauthManager.swift
+++ MultiAccountOauth/OauthManager.swift
@@ -22,7 +22,7 @@ public class OauthManager: DynamicStorage {
     
     public var oauthSession: OIDAuthorizationFlowSession?
 
-    dynamic var signinUsers = [String: [String: String]]()
+    dynamic var signinUsersRefreshToken = [String: String]()
     
     public func configure(cliendId: String, scope: [String], urlScheme: String, serverCliendId: String?) {
         self.cliendId = cliendId
@@ -54,10 +54,10 @@ public class OauthManager: DynamicStorage {
         
         oauthSession = OIDAuthState.authState(byPresenting: request, presenting: controller) { (state, error) in
             if state != nil && state!.isAuthorized {
-                let idToken = state?.value(forKey: "idToken")! as? String
+                let idToken = state?.value(forKey: "idToken")! as! String
                 let refreshToken = state?.refreshToken!
                 let serverToken = state?.lastTokenResponse?.additionalParameters?["server_code"] as? String
-                let accessToken = state?.value(forKey: "accessToken")! as? String
+                let accessToken = state?.value(forKey: "accessToken")! as! String
                 ExternalRequest.sendExternalRequest(url: "https://www.googleapis.com/oauth2/v2/userinfo", method: .get, param: ["access_token": accessToken as AnyObject, "alt": "json" as AnyObject], completion: { (json) in
                     if json["error"] == JSON.null {
                         print(json)
@@ -65,8 +65,8 @@ public class OauthManager: DynamicStorage {
                         let id = json["id"].stringValue
                         let profile = json["picture"].stringValue
                         let email = json["email"].stringValue
-                        self.signinUsers[id] = ["refreshToken": refreshToken!]
-                        let instance = GoogleUserInstance(email: email, name: name, id: id, profile: profile, refresh_token: refreshToken!, id_token: idToken!, access_token: accessToken, server_token: serverToken)
+                        self.signinUsersRefreshToken[email] = refreshToken!
+                        let instance = GoogleUserInstance(email: email, name: name, id: id, profile: profile, refresh_token: refreshToken!, id_token: idToken, access_token: accessToken, server_token: serverToken)
                         if self.userWithEmail(email: email) == nil {
                             self.authenticatedUsers.append(instance)
                         }
@@ -81,10 +81,41 @@ public class OauthManager: DynamicStorage {
         }
     }
     
-    public func backgroundSigninUserSilently(refresh_token: String, completion: @escaping ((_ success: Bool, _ result: JSON) -> ())) {
+    public func signinAllUsersSilently() {
+        for email in signinUsersRefreshToken.keys {
+            backgroundSigninUserSilently(refresh_token: signinUsersRefreshToken[email]!, email: email, completion: nil)
+        }
+    }
+    
+    public func backgroundSigninUserSilently(refresh_token: String, email: String? = nil, completion: ((_ success: Bool, _ user: GoogleUserInstance?, _ error: OauthError?) -> ())?) {
         let refreshRequest = OIDTokenRequest(configuration: getConfiguration(), grantType: OIDGrantTypeRefreshToken, authorizationCode: nil, redirectURL: URL(string: OauthManager.sharedInstance.redirectURL)!, clientID: OauthManager.sharedInstance.cliendId, clientSecret: nil, scopes: OauthManager.sharedInstance.scope, refreshToken: refresh_token, codeVerifier: nil, additionalParameters: nil)
         ExternalRequest.sendExternalURLRequest(urlRequest: refreshRequest.urlRequest(), completion: { (result) in
-            completion(result["error"] == JSON.null, result)
+            if result["error"] == JSON.null {
+                let id_token = result["id_token"].stringValue
+                let access_token = result["access_token"].stringValue
+                ExternalRequest.sendExternalRequest(url: "https://www.googleapis.com/oauth2/v2/userinfo", method: .get, param: ["access_token": access_token as AnyObject, "alt": "json" as AnyObject], completion: { (json) in
+                    if json["error"] == JSON.null {
+                        print(json)
+                        let name = json["name"].stringValue
+                        let id = json["id"].stringValue
+                        let profile = json["picture"].stringValue
+                        let email = json["email"].stringValue
+                        let instance = GoogleUserInstance(email: email, name: name, id: id, profile: profile, refresh_token: refresh_token, id_token: id_token, access_token: access_token, server_token: nil)
+                        if self.userWithEmail(email: email) == nil {
+                            self.authenticatedUsers.append(instance)
+                        }
+                        completion?(true, instance, nil)
+                    } else {
+                        completion?(false, nil, OauthError.UnKnowError(json))
+                    }
+                })
+            } else {
+                if result["code"] == 401 {
+                    completion?(false, nil, OauthError.RefreshTokenInvalid)
+                } else {
+                    completion?(false, nil, OauthError.UnKnowError(result))
+                }
+            }
         })
     }
     
@@ -94,5 +125,12 @@ public class OauthManager: DynamicStorage {
         let configuration = OIDServiceConfiguration(authorizationEndpoint: authorizationEndpoint!, tokenEndpoint: tokenEndpoint!)
         return configuration
     }
+    
+}
+
+public enum OauthError {
+    
+    case RefreshTokenInvalid
+    case UnKnowError(JSON)
     
 }
